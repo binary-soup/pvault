f60b5be0f12e37c79be75f3f207cc655912cbee5
Ryan Stamp <apps@binarysoup.dev>
2025-07-24 00:32:13 -0400

    Don't run init in post install script (bad practice)

--- install/config/postinst.sh
+++ install/config/postinst.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-set -e # exit on error
-
-# detect active user
-USER=${SUDO_USER:-$(logname)}
-echo -e "Installing for \e[1m$USER\e[0m"
-
-# run init command at user level (not root)
-runuser -l "$USER" -c '/usr/local/bin/pvault init'
+echo -e 'USER: run "\e[1mpvault init\e[0m" to finish setup'
